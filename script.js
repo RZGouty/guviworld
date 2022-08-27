559ac20c28975ce3a256d0344151fa1a7c719c33
Rui Cabrita <111704969+RZGouty@users.noreply.github.com>
2022-08-27 08:23:52 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
   document.getElementById("days").innerHTML = days;
   document.getElementById("hours").innerHTML = hours;
@@ -27,6 +27,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("timer").innerHTML = "EXPIRED";
   }
 }, 1000);
