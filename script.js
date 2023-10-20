42ef93b60fb56d8074524edb1ed41c95113ed009
Rui Cabrita <111704969+RZGouty@users.noreply.github.com>
2023-10-20 07:26:28 +0100

    Update script.js
    Release date updat

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 1, 2023 00:00:00:01").getTime();
+var countDownDate = new Date("Jan 1, 2025 00:00:00:01").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
